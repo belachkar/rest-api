39fec98aa2dd19edb60e06be6894df830f755c78
belachkar ali <34995109+belachkar@users.noreply.github.com>
2019-07-16 23:29:09 +0100

    Add MongoDB  connection

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const MDB = {
+  prtl: process.env.MDB_PROTOCOLE,
+  username: process.env.MDB_USER,
+  password: process.env.MDB_PASS,
+  host: process.env.MDB_HOST,
+};
+
+const urlMDB = MDB.prtl+'://'+MDB.username+':'+MDB.password+'@'+MDB.host;
+
+const postsRoute = require('./routes/posts');
+
+// Middlewares
+app.use(bodyParser.json());
 
 // Routes
+app.use('/posts', postsRoute);
 app.get('/', (req, res) => {
   res.send('We are on home');
+});
+
+// DB Connection
+mongoose.connect(urlMDB, { useNewUrlParser: true, ssl: true }, (err) => {
+  if (!err) {
+    console.log('Connnected to DB!');
+  } else {
+    console.log(urlMDB);
+    console.log(err);
+  }
 });
 
 // Listening
---
+++ routes/posts.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/post');
+
+router.get('/', (req, res) => {
+  res.send('We are on POSTS');
+});
+
+router.post('/', (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description
+  });
+  console.log(req.body);
+
+  post.save()
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json({ Error: err });
+    });
+});
+
+module.exports = router;
