2c94357b477d1bd1e4c2e4c22b15bf3ec1668b67
belachkar ali <34995109+belachkar@users.noreply.github.com>
2019-07-18 16:38:24 +0100

    Add CRUD Operations

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
 const bodyParser = require('body-parser');
+
 require('dotenv').config();
+const postsRoute = require('./routes/posts');
 
 const MDB = {
   username: process.env.MDB_USER,
   password: process.env.MDB_PASS,
   prtl: process.env.MDB_PROTOCOLE_OLD,
   host: process.env.MDB_HOST_OLD,
+  // prtl: process.env.MDB_PROTOCOLE,
+  // host: process.env.MDB_HOST,
   options: {
-    // dbName: 'test',
-    ssl: true,
+    dbName: 'test',
     useNewUrlParser: true,
-    authSource: 'admin'
+    authSource: 'admin',
+    ssl: true,
+    keepAlive: true
   }
 };
 
 const urlMDB = MDB.prtl + '://' + MDB.username + ':' + MDB.password + '@' + MDB.host;
 
-const postsRoute = require('./routes/posts');
 
 // Middlewares
+app.use(cors());
 app.use(bodyParser.json());
 
 // Routes
@@ -34,19 +40,15 @@ app.get('/', (req, res) => {
 mongoose.connect(urlMDB, MDB.options);
 const mdb = mongoose.connection;
 
-mdb.on('error', (err) => console.error('ERROR on connecting to mongoDB:\n', err));
-mdb.once('open', () => {
-  console.log('Connnection to MongoDB SUCCESS');
-});
-// mongoose.connect(urlMDB, { useNewUrlParser: true, ssl: true }, (err) => {
-//   if (!err) {
-//     console.log('Connnected to DB!');
-//   } else {
-//     console.log(urlMDB);
-//     console.log(err);
-//   }
-// });
-
+mdb
+  .on('connecting', () => console.log('connecting to mongoDB...'))
+  .on('disconnecting', () => console.log('disconnecting from mongoDB server...'))
+  .on('disconnected', () => console.log('disconnected from mongoDB server.'))
+  .on('close', () => console.log('Connection to mongoDB server closed.'))
+  .on('error', (err) => console.error('ERROR on connecting to mongoDB server:\n', err))
+  .once('open', () => {
+    console.log('Connnection to MongoDB SUCCESS');
+  });
 
 // Listening
 app.listen(3000);
--- routes/posts.js
+++ routes/posts.js
@@ -2,24 +2,79 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/post');
 
-router.get('/', (req, res) => {
-  res.send('We are on POSTS');
+// Get back all the posts
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({ Error: err });
+  }
 });
 
-router.post('/', (req, res) => {
+// Submit a post
+router.post('/', async (req, res) => {
   const post = new Post({
     title: req.body.title,
     description: req.body.description
   });
-  console.log(req.body);
 
-  post.save()
-    .then(data => {
-      res.json(data);
-    })
-    .catch(err => {
-      res.json({ Error: err });
-    });
+  try {
+    const savedPost = await post.save();
+    console.log(savedPost);
+    res.json(savedPost);
+  } catch (err) {
+    res.json({ Error: err });
+  }
+});
+
+// Get specific post
+router.get('/post/:id', async (req, res) => {
+  const id = req.params.id ? req.params.id : '';
+
+  try {
+    const post = await Post.findOne({ _id: id });
+    res.json(post);
+  } catch (err) {
+    console.error('Error getting post:', err);
+    res.json({ Error: 'Unable to get the post, be sure the id of the post is correct!' });
+  }
+});
+
+// Delete a specific post
+router.delete('/:id', async (req, res) => {
+  const id = req.params.id ? req.params.id : '';
+
+  try {
+    const post = await Post.deleteOne({ _id: id });
+    res.json({ message: post });
+  } catch (err) {
+    console.error('Error deleting post:', err);
+    res.json({ Error: 'Unable to delete the post, be sure the the post exists!' });
+  }
+});
+
+// Update a post
+router.patch('/:id', async (req, res) => {
+  const id = req.params.id ? req.params.id : '';
+  const title = req.body.title;
+  const description = req.body.description;
+
+  const update = {};
+  if (title) update.title = title;
+  if (description) update.description = description;
+
+  if (Object.keys(update).length && Object.keys(update).length > 0) {
+    try {
+      const post = await Post.updateOne({ _id: id }, update);
+      res.json({ message: post });
+    } catch (err) {
+      console.error('Error updating post:', err);
+      res.json({ Error: 'Unable to update the post, be sure the the post exists!' });
+    }
+  } else {
+    res.json({ Error: 'Post not updated, no think to update!' });
+  }
 });
 
 module.exports = router;
